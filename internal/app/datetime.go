75e4e296051f5940ba4d00ec93071b95a1ba26a7
James Chen <jameschan312.cn@gmail.com>
2026-01-13 23:11:28 -0500

    feat: "ParseDateTimeString" function to convert datetime string into "time.Time" object

--- internal/app/datetime.go
+++ internal/app/datetime.go
@@ -1,8 +1,51 @@
 package app
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 // GetDateTimeString formats a time.Time into a human-readable string.
 func GetDateTimeString(t time.Time) string {
 	return t.Local().Format("2006-01-02 15:04:05 MST")
+}
+
+func ParseDateTimeString(dateTimeStr string) (time.Time, error) {
+	const (
+		dateLayout     = "2006-01-02"
+		dateTimeLayout = "2006-01-02 15:04:05"
+	)
+
+	var (
+		t   time.Time
+		err error
+	)
+
+	switch len(dateTimeStr) {
+	case len(dateLayout):
+		// Date only → default to 23:59:59 local time
+		dateTimeStr = dateTimeStr + " 23:59:59"
+		t, err = time.ParseInLocation(dateTimeLayout, dateTimeStr, time.Local)
+
+	case len(dateTimeLayout):
+		// Date + time → parse as local time
+		t, err = time.ParseInLocation(dateTimeLayout, dateTimeStr, time.Local)
+
+	default:
+		return time.Time{}, fmt.Errorf(
+			"invalid time format %q "+
+				"(expected YYYY-MM-DD or YYYY-MM-DD hh:mm:ss)",
+			dateTimeStr,
+		)
+	}
+
+	if err != nil {
+		return time.Time{}, fmt.Errorf(
+			"parse local time %q: %w",
+			dateTimeStr,
+			err,
+		)
+	}
+
+	return t.UTC(), nil
 }
