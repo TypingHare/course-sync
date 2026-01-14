0acd9917d045345aac580093dfa2053036ebcd05
James Chen <jameschan312.cn@gmail.com>
2026-01-13 23:11:53 -0500

    feat: student repos (in master repo) and some related function

--- internal/app/student.go
+++ internal/app/student.go
@@ -1,18 +1,35 @@
 package app
 
 import (
+	"fmt"
 	"path/filepath"
 	"strings"
 
 	"github.com/TypingHare/course-sync/internal/adapter/repo/jsonstore"
+	"github.com/TypingHare/course-sync/internal/domain/model"
 	"github.com/TypingHare/course-sync/internal/domain/service"
+	"github.com/TypingHare/course-sync/internal/support/filesystem"
 	"github.com/iancoleman/strcase"
 )
 
 const StudentDataFileName = "students.json"
 
+// StudentRepoBaseDir is the base directory for local student repositories in
+// the master repository
+const StudentRepoBaseDir = "student-repos/"
+
 func GetStudentDataFile(dataDir string) string {
 	return filepath.Join(dataDir, StudentDataFileName)
+}
+
+// GetStudentRepoDir constructs the local path for a student's repository
+// based on the base directory and the student's directory name.
+func GetStudentRepoDir(projectDir string, studentName string) string {
+	return filepath.Join(
+		projectDir,
+		StudentRepoBaseDir,
+		GetStudentDirName(studentName),
+	)
 }
 
 func GetStudentService(
@@ -34,4 +51,30 @@ func GetStudentFiles(dataDir string) []string {
 	return []string{
 		filepath.Join(dataDir, SubmissionDataFileName),
 	}
+}
+
+func DistributeFileToStudentRepos(
+	students []model.Student,
+	projectDir string,
+	relPath string,
+) error {
+	var studentRepoDirs []string
+	for i := range students {
+		studentRepoDirs = append(
+			studentRepoDirs,
+			GetStudentRepoDir(projectDir, students[i].Name),
+		)
+	}
+
+	absPath := filepath.Join(projectDir, relPath)
+
+	for i := range studentRepoDirs {
+		destPath := filepath.Join(studentRepoDirs[i], relPath)
+		err := filesystem.CopyFile(absPath, destPath)
+		if err != nil {
+			return fmt.Errorf("copy file to %s: %w", destPath, err)
+		}
+	}
+
+	return nil
 }
